3e9bf3b7e7d7f52bea39e270879f49203eb7f4f1
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-30 00:35:47 +0900

    필드 추가 하는 법

--- videos/serializers.py
+++ videos/serializers.py
@@ -1,17 +1,22 @@
 #전부 rest_framework에서 온다.
-
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework import routers, serializers, viewsets, permissions
 
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-from .models import Video
+from .models import Video, Category
 
 
-#JSON Serializer로 만드는 부분
+
 class VideoSerializer(serializers.HyperlinkedModelSerializer):
+    category_title = serializers.CharField(source='category.title', read_only=True)
+    # category_url = serializers.CharField(source='category.get_absolute_url',read_only=True)
+    category_image = serializers.CharField(source='category.get_image_url', read_only=True)
+    #  미리 정의된 method가 있어야한다.
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     class Meta:
         model = Video
+        # 여기에 추가된것들이 JSON으로 전달된다.
         fields = [
             'id',
             'slug',
@@ -20,6 +25,34 @@ class VideoSerializer(serializers.HyperlinkedModelSerializer):
             'embed_code',
             'share_message',
             'timestamp',
+            'category_title',
+            'category',
+            'category_image',
+            # 'category_url',
+        ]
+
+#JSON Serializer로 만드는 부분
+class VideoSerializer(serializers.HyperlinkedModelSerializer):
+    category_title = serializers.CharField(source='category.title', read_only=True)
+    # category_url = serializers.CharField(source='category.get_absolute_url',read_only=True)
+    category_image = serializers.CharField(source='category.get_image_url', read_only=True)
+    #  미리 정의된 method가 있어야한다.
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    class Meta:
+        model = Video
+        # 여기에 추가된것들이 JSON으로 전달된다.
+        fields = [
+            'id',
+            'slug',
+            'title',
+            'order',
+            'embed_code',
+            'share_message',
+            'timestamp',
+            'category_title',
+            'category',
+            'category_image',
+            # 'category_url',
         ]
 
 
