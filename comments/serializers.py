df2329e6d25fe65293f02eaf227ad507db607206
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-30 00:50:56 +0900

    PrimaryKeyField로 작성 User 추가하기

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,13 +1,19 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers, viewsets
 
 from .models import Comment
 
+User = get_user_model()
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     class Meta:
         model = Comment
         fields = [
             'id',
+            'parent',
+            'user',
             'video',
             'text'
         ]
