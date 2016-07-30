9c12e0e8387d92ba87d44cf543fb6090cd784121
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-30 21:46:49 +0900

    [new] Free Preview Add

--- videos/permissions.py
+++ videos/permissions.py
@@ -7,8 +7,14 @@ class IsMember(permissions.BasePermission):
     Checks if the user is a member of the service
     """
     def has_permission(self, request, view):
-        if request.user.is_authenticated():
-            if request.user.is_member:
+        print(view.get_object().free_preview)
+        if request.user.is_authenticated() or view.get_object().free_preview:
+            try:
+                is_member = request.user.is_member
+            except:
+                is_member = None
+
+            if is_member or view.get_object().free_preview:
                 return True
             else:
                 raise PermissionDenied("You must be a member to view this.")
