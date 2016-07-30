1bde541a27b6e28ca4f28a806abed45c2e2af3b8
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-30 21:16:30 +0900

    [new] 결제 토큰 내용

--- srvup/utils.py
+++ srvup/utils.py
@@ -4,3 +4,4 @@ def jwt_response_payload_handler(token, user=None, *args, **kwargs):
         "user": str(user.username),
         "active": user.is_active
     }
+# 만약에 결제 모듈을 붙인다면, 여기다가 token을 처음로그인 할 때 받아내야 한다.
